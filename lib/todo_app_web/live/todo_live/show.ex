8cce375e0739664fd3772a121f089b955b5fa7ea
Saeem Mahmood <saimmahmood61@gmail.com>
2023-09-20 02:35:46 +0500

    modifications in show page and its edit functionality

--- lib/todo_app_web/live/todo_live/show.ex
+++ lib/todo_app_web/live/todo_live/show.ex
@@ -1,19 +1,18 @@
 defmodule TodoAppWeb.TodoLive.Show do
+  alias TodoApp.Accounts
   use TodoAppWeb, :live_view
 
-  alias TodoApp.Items
+  alias TodoApp.{Items, Accounts}
 
   @impl true
-  def mount(_params, _session, socket) do
-    {:ok, socket}
-  end
-
-  @impl true
-  def handle_params(%{"id" => id}, _, socket) do
-    {:noreply,
-     socket
-     |> assign(:page_title, page_title(socket.assigns.live_action))
-     |> assign(:todo, Items.get_todo!(id))}
+  def mount(%{"id" => id}, %{"user_token" => user_token}, socket) do
+    {
+      :ok,
+      socket
+      |> assign(:current_user, Accounts.get_user_by_session_token(user_token))
+      |> assign(:page_title, page_title(socket.assigns.live_action))
+      |> assign(:todo, Items.get_todo!(id))
+    }
   end
 
   defp page_title(:show), do: "Show Todo"
